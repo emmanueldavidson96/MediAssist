89f253d196b3d79d33ff80bb5649062c9f83a289
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-13 23:14:45 +0100

    Added My Banner

---
+++ app/components/BannerArea.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import doctorImage from "../assets/a3618752832d002a51cb8729895a364c.jpg";
+
+export default function BannerArea() {
+  return (
+    <div className='flex h-fit justify-between w-[90%] m-auto'>
+        <div className='flex flex-col gap-4 w-[50%] pt-48 '>
+            <h2 className='text-black text-3xl font-extrabold'>Find & Book Appointment with your Doctor In a Seamless Experience</h2>
+            <p className='text-black'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus 
+                laboriosam, possimus illum, amet adipisci incidunt tempore corrupti qui magnam quas. 
+                Nisi labore consequatur tenetur tempora. Dignissimos ea et dicta.
+            </p>
+            <button className='w-fit h-fit pt-3 pb-3 pl-6 pr-6 text-white bg-blue-500'>Explore Now</button>
+        </div>
+        <div className='flex items-center justify-center w-[50%] pt-8'>
+            <Image src={doctorImage} width={450} height={600} className='rounded-md'/>
+        </div>
+
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import BannerArea from "./components/BannerArea";
 
 export default function Home() {
   return (
     <main className="bg-white">
        <Navbar />
+       <BannerArea/>
     </main>
   );
 }
