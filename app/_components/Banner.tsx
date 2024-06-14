c0a39838f47912436efe381530e005ce0cde94a9
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-14 20:35:35 +0100

    Added Banner Content

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 export default function Banner() {
   return (
-    <section>
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <section className='w-full h-fit'>
+        <div className="m-auto w-[90%] px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
             <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
             <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
                 <Image
@@ -17,19 +17,19 @@ export default function Banner() {
             </div>
 
             <div className="lg:py-24">
-                <h2 className="text-4xl font-extrabold sm:text-4xl">Grow your audience</h2>
+                <h2 className="text-4xl font-extrabold sm:text-4xl">Find, <span className='text-blue-500'>Interact</span> & Book Appointments with Your <span className='text-blue-500'>Doctor</span> in a <span className='text-blue-500'>specialized experience.</span></h2>
 
-                <p className="mt-4 text-gray-600">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-                eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
-                quidem quam repellat.
+                <p className="mt-4 text-gray-600 text-sm leading-6">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+                    eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
+                    quidem quam repellat.
                 </p>
 
                 <a
                     href="#"
-                    className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                    className="mt-8 inline-block rounded bg-blue-500 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
                 >
-                    Get Started Today
+                    Explore Now
                 </a>
             </div>
             </div>
