d0aad8d79262571ac50e93c25b8e62ab39e519be
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-14 21:42:00 +0100

    Popular Doctors Section

--- app/_components/PopularDoctors.tsx
+++ app/_components/PopularDoctors.tsx
@@ -1,10 +1,13 @@
+"use client";
 import React from 'react'
+import imageDoc from "../../public/assets/dc04aaae4d9a84ad7c4a3be7bc4e9766.jpg";
+import imageDoc2 from "../../public/assets/doctors_visit_1200x900.jpg";
 
 export default function PopularDoctors() {
   return (
     <div className='flex flex-col'>
         <h1 className='text-3xl'>Popular Doctors</h1>
-        
+
 
     </div>
   )
