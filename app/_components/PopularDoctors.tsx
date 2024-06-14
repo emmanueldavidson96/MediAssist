d633e4294cd20bff89af51e27a230dfaa4e7c3e2
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-14 21:29:05 +0100

    Popular Doctor Section

---
+++ app/_components/PopularDoctors.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PopularDoctors() {
+  return (
+    <div className='flex flex-col'>
+        <h1 className='text-3xl'>Popular Doctors</h1>
+        
+
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Banner from "./_components/Banner";
 import CategorySearch from "./_components/CategorySearch";
+import PopularDoctors from "./_components/PopularDoctors";
 
 export default function Home() {
   return (
     <section className="h-fit w-full">
       <Banner/>
       <CategorySearch/>
+      <PopularDoctors/>
 
     </section>
     
