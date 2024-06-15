479cff4a123cb05136fb59d8ed797a0fe19b5971
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-15 23:10:37 +0100

    Medical History

--- app/upload-medical-history/page.tsx
+++ app/upload-medical-history/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MedicalHistory from '../_components/_ui/MedicalHistory'
 
 export default function UploadMedicalHistory() {
   return (
-    <div className=''>
-
+    <div className='w-[90%] m-auto h-screen flex items-center justify-center mt-[-100px]'>
+        <MedicalHistory />
     </div>
   )
 }
