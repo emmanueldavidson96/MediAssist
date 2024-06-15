6d5b1af9b8449d0ad6fbfca685348d5e5e990f0d
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-15 23:22:45 +0100

    Medical History Form Fields

--- app/_components/_ui/MedicalHistory.tsx
+++ app/_components/_ui/MedicalHistory.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
+import { CalendarForm } from './CalenderFormField'
 export default function MedicalHistory() {
   return (
-    <form>
-
-    </form>
+    <div className='w-[50%] h-fit flex flex-col gap-4 shadow-sm items-center p-4'>
+        <h1 className='text-3xl font-bold '>Medical History Upload</h1>
+        <CalendarForm/>
+        
+        <Button className='w-full bg-blue-500 hover:bg-blue-700'>
+            Upload Medical History
+        </Button>
+    </div>
   )
 }
