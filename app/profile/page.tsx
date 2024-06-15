4903255c7237a78ad887fc60479e252ff9946c5e
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-15 22:25:14 +0100

    Dashboard Designs

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,12 +8,11 @@ export default function Profile() {
         <div className='w-[20%] h-full bg-white'>
         </div>
         <div className='w-[80%] h-full bg-blue-400 rounded-3xl flex flex-col items-center pt-6 gap-4 pb-6'>
-            <div className='w-[95%] h-[350px] bg-blue-100 rounded-3xl shadow-md p-8 flex flex-col gap-3'>
+            <div className='w-[95%] h-fit bg-blue-100 rounded-3xl shadow-md p-8 flex flex-col gap-3'>
                 <h1 className='text-2xl border-l-4 border-l-blue-500 pl-2 font-bold'>Patient Details</h1>
                 <div className='w-full h-fit flex '>
                     <div className=''>
                         <Image src={imagePatient} width={150} height={150} alt='' className='rounded-full'/>
-
                     </div>
                     <div className='ml-8 mt-2 flex flex-col gap-8'>
                         <h2 className='text-lg font-bold '>Nathan Brokleen</h2>
@@ -32,28 +31,26 @@ export default function Profile() {
                             </div>
                         </div>
                     </div>
-
-                    <div className=''>
-                        <div>
-
-                        </div>
-                        <div>
-
-                        </div>
-                        <div>
-
-                        </div>
-                        <div>
-                            
-                        </div>
+                </div>
+                <div className='flex gap-8'>
+                    <div className='w-[200px] h-[120px] bg-blue-500 shadow-lg rounded-3xl hover:bg-blue-600 cursor-pointer'>
 
                     </div>
+                    <div className='w-[200px] h-[120px] bg-blue-500 shadow-lg rounded-3xl hover:bg-blue-600 cursor-pointer'>
 
+                    </div>
+                    <div className='w-[200px] h-[120px] bg-blue-500 shadow-lg rounded-3xl hover:bg-blue-600 cursor-pointer'>
+
+                    </div>
+                    <div className='w-[200px] h-[120px] bg-blue-500 shadow-lg rounded-3xl hover:bg-blue-600 cursor-pointer'>
+
+                    </div>
                 </div>
 
             </div>
             <div className='w-[95%] h-[600px] flex justify-between'>
-                <div className='bg-blue-100 h-full w-[45%] rounded-3xl shadow-md'>
+                <div className='bg-blue-100 h-full w-[45%] rounded-3xl shadow-md p-8 flex flex-col gap-3'>
+                    <h1 className='text-xl border-l-4 border-l-blue-500 pl-2 font-bold'>Medical History</h1>
                     
                     
 
