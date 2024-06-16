7c2d3239ff24a9b6c9631ec6d21e473e6d47b951
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-16 22:16:21 +0100

    Pipeops commit

---
+++ app/getmedication/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GetMedication() {
+  return (
+    <div>page</div>
+  )
+}
---
+++ app/makeanappointment/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MakeAnAppointment() {
+  return (
+    <div>make an appointment</div>
+  )
+}
