1c04475961dbce3789c2bff14fa7082066078969
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-16 22:26:45 +0100

    Pipeops commit

--- app/userprofile/page.tsx
+++ app/userprofile/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function UserProfile() {
   return (
-    <div>UserProfile</div>
+    <div className='w-[90%] m-auto h-screen flex items-center justify-center mt-[-100px]'>
+        
+    </div>
   )
 }
