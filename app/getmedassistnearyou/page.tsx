bd08bfa099032ae737fc416597ea5017665ae870
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-17 21:40:31 +0100

    Integration with Gemini AI

---
+++ app/getmedassistnearyou/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GetMediAssistNearYou() {
+  return (
+    <div>page</div>
+  )
+}
---
+++ app/mediassistsocial/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MediAssistSocial() {
+  return (
+    <div>page</div>
+  )
+}
