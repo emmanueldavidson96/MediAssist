b8248c13410533c5dae9dcc216fd4225cf5b296c
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-17 21:59:06 +0100

    Get MediAssist

--- app/_components/_ui/MedicalHistory.tsx
+++ app/_components/_ui/MedicalHistory.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import { CalendarForm } from './CalenderFormField'
+
 export default function MedicalHistory() {
   return (
     <div className='w-[50%] h-fit flex flex-col gap-1 shadow-sm items-center p-4'>
-        <h1 className='text-3xl font-bold '>Medical History Upload</h1>
+        <h1 className='text-3xl font-bold '>Upload Your Medical History</h1>
         <CalendarForm/>
     </div>
   )
