ddaad6e3ca171303cce244f9bd1554d4e1f75292
Emmanuel <emmanueldavidson524@gmail.com>
2024-06-18 23:24:58 +0100

    Pipeops Commit

--- app/_components/_ui/MedicalHistory.tsx
+++ app/_components/_ui/MedicalHistory.tsx
@@ -5,7 +5,7 @@ import { CalendarForm } from './CalenderFormField'
 export default function MedicalHistory() {
   return (
     <div className='w-[50%] h-fit flex flex-col gap-1 shadow-sm items-center p-4'>
-        <h1 className='text-3xl font-bold '>Upload </h1>
+        <h1 className='text-3xl font-bold '>Upload History </h1>
         <CalendarForm/>
     </div>
   )
